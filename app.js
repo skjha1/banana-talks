9bddc1f326f4d74be7086c73fcde0165ce91cdb3
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-08-04 13:45:51 +0530

    taking input and console output js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate"); // document.querysel is a api call
+var txtInput = document.querySelector("#txt-input")
+console.log(txtInput)
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked");
-})
+function clickEventHandler(){
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+}
+btnTranslate.addEventListener("click", clickEventHandler)
