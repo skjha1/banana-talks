0707cc4d473dfdcb602bc658ccfc34c755a2a22e
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-08-04 16:17:10 +0530

    Checking either working or not

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
-var btnTranslate = document.querySelector("#btn-translate"); // document.querysel is a api call
-var txtInput = document.querySelector("#txt-input")
-var outputDiv = document.querySelector("#output")
-
- //var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output');
 
 var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
-// translation function
-function getTranslationURL(text) {
-    return serverUrl + "?" + "text=" + input;
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text
 }
 
-// error handaler function
 
-function errorHandaler(error){
-    console.log("error occured",error)
-    alert("Something wrong with server : try again after sometime ")
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("Something wrong with server please try after sometime!!!");
 }
 
-function clickHandler() {
 
-    var inputText = txtInput.value; // taking input 
+function clickEventHandler() {
+    var inputText = txtInput.value; //taking input
 
-    // calling server for processing  // server is processing 
-
-    fetch(getTranslationURL(inputText))
+    fetch(getTranslationUrl(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output 
+            outputDiv.innerText = translatedText;
         })
-        .catch(errorHandaler) // handaling error
+        .catch(errorHandler)
 
 }
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickEventHandler)
