7baa7255861eb9adb4e68e12e26ad4e32ca557e0
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-08-04 15:26:56 +0530

    fetching api and sending data to server

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate"); // document.queryse
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
-//outputDiv.innerText= "Shivendra"
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+// translation function
+function getTranslationURL(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+
+// error handaler function
+
+
 
 console.log(txtInput)
 
-function clickEventHandler(){
-    outputDiv.innerText= "Banana language " + txtInput.value; // reading output 
+function clickEventHandler() {
+
+
+    var inputText = txtInput.value; // taking input 
+
+    // calling server for processing 
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+        
+
 }
 btnTranslate.addEventListener("click", clickEventHandler)
