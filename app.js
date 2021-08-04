9e99122e0ca3a3c7760c4a06733d823ccd152cfb
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-08-04 12:53:17 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Hey this is for testing script file from another file ")
+alert("These scripts works")
+var username = prompt("Give me your username")
+alert("This scripts works " + username)
