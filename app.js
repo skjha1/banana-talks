d16c38b535ba4a457d16d8f903e1c3dafa2ed832
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-08-04 14:04:08 +0530

    exercise 4

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ function clickEventHandler(){
     console.log("input",txtInput.value);
 }
 btnTranslate.addEventListener("click", clickEventHandler)
+
+1. document.querySelector("textarea");
+2. .btn-primary
+3. #input-btn
+4. "input [name='translator']"
